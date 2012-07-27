3eac73513953c6c5793682ef2b56ff1b33eb1d66
Damien Mirand <dm@mirabel-sil.com>
2012-07-27 14:51:05 +0200

    CrmFileFieldDesc implements Cloneable

--- src/za/dams/paracrm/CrmFileTransaction.java
+++ src/za/dams/paracrm/CrmFileTransaction.java
@@ -127,7 +127,7 @@ public class CrmFileTransaction {
     	}
 	}
 	
-	public static class CrmFileFieldDesc {
+	public static class CrmFileFieldDesc implements Cloneable {
     	public FieldType fieldType ;
     	public String fieldLinkBible ;
     	public String fieldCode ;
@@ -150,6 +150,20 @@ public class CrmFileTransaction {
     		this.fieldName = fieldName ;
     		this.fieldIsPivot = fieldIsPivot ;
     		this.fieldIsReadonly = fieldIsReadonly ;
+    	}
+    	public CrmFileFieldDesc clone() {
+    		Object o = null;
+    		try {
+    			// On récupère l'instance à renvoyer par l'appel de la 
+    			// méthode super.clone()
+    			o = super.clone();
+    		} catch(CloneNotSupportedException cnse) {
+    			// Ne devrait jamais arriver car nous implémentons 
+    			// l'interface Cloneable
+    			// cnse.printStackTrace(System.err);
+    		}
+    		// on renvoie le clone
+    		return (CrmFileFieldDesc)o;
     	}
 	}
 	
