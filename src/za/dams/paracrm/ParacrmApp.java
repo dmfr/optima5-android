bae1ab8a4bcddf00966c9bb237601e67b8557bef
Damien Mirand <dm@mirabel-sil.com>
2013-06-26 17:16:07 +0200

    ACRA crash report : delete

--- src/za/dams/paracrm/ParacrmApp.java
+++ src/za/dams/paracrm/ParacrmApp.java
@@ -1,16 +1,16 @@
 package za.dams.paracrm;
 
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
+//import org.acra.ACRA;
+//import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dDBQdV90bHdhQXE3MHAxNVNwWWFWaEE6MQ") 
+//@ReportsCrashes(formKey = "dDBQdV90bHdhQXE3MHAxNVNwWWFWaEE6MQ") 
 public class ParacrmApp extends Application {
 	@Override
     public void onCreate() {
         // The following line triggers the initialization of ACRA
-        ACRA.init(this);
+        //ACRA.init(this);
         super.onCreate();
     }
 }
